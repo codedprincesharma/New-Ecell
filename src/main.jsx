3a79c510b607e884013c8d6147b780d06afc3a66
Prince Sharma <hit.princeraj@gmail.com>
2025-07-01 22:47:38 +0530

    google analatic

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,10 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
-import { Analytics } from "@vercel/analytics/react";
 createRoot(document.getElementById("root")).render(
-  <Analytics>
+  
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </Analytics>
+  
 );
