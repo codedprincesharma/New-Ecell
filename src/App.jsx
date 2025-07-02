42b33ee43e446d68502daf9e48961eb6c9b9fed6
Prince Sharma <hit.princeraj@gmail.com>
2025-07-02 23:34:27 +0530

    add locomotive js

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
 import { Router } from "react-router-dom";
 import Navbar from "./component/Navbar";
 import MAinroutes from "./routes/MAinroutes";
+import { useEffect } from "react";
+import LocomotiveScroll from "locomotive-scroll";
 const App = () => {
+  useEffect(() => {
+    const scroll = new LocomotiveScroll({
+      el: document.querySelector("[data-scroll-container]") || document.body,
+      smooth: true,
+    });
+
+    return () => {
+      scroll.destroy();
+    };
+  }, []);
   return (
-    <>
+    <div data-scroll-container>
       <Navbar />
       <MAinroutes />
-    </>
+    </div>
   );
 };
 
